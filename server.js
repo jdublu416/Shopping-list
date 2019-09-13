47c3e8b280cdef9a8362f896f9b9ea05ad6e62c0
Janet Warren <jewljbl13@aol.com>
2019-09-13 13:00:57 -0400

    deployment changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -13,14 +14,23 @@ app.use(bodyParser.json());
 
 //db congfiguration
 const db = require('./config/keys').mongoURI;
-+
-//connect to db
++//connect to db
 mongoose
-  .connect(db, {useNewUrlParser: true})
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 //use routes
 app.use('/api/items', items);
+
+//serve static assest in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  //set the path
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
